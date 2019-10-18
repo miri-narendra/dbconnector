d50aad60f8cf52b40c4f7d502853013f8b70aa98
miri-narenrda <narendra.sharma@miritech.com>
2019-10-18 12:50:31 +0530

    Map stopped

--- materialmaster/src/app/maps/maps.component.ts
+++ materialmaster/src/app/maps/maps.component.ts
@@ -19,7 +19,7 @@ export class MapsComponent implements OnInit {
 
   ngOnInit() {
 
-    var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
+    /*var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
     var mapOptions = {
         zoom: 13,
         center: myLatlng,
@@ -117,6 +117,7 @@ export class MapsComponent implements OnInit {
         position: myLatlng,
         title: "Hello World!"
     });
+    */
 
     // To add the marker to the map, call setMap();
     //marker.setMap(map);
